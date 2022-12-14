26fc96e4e2dbc38ac3a35757f1c31575efeb7a7d
azhe <10846006@ntub.edu.tw>
2022-12-14 22:51:05 +0800

    refactor

---
+++ js/main.js
@@ -0,0 +1,37 @@
+function change_pic1() {
+    document.getElementsByClassName("test").innerHTML = "<img src='image/double12.png'>";
+}
+
+function change_pic2() {
+    document.getElementsByClassName("test").innerHTML = " ";
+}
+function change_pic3() {
+    document.getElementsByClassName("main_container_slider").innerHTML = "<img src='image/main3.jpg'>";
+}
+function change_pic4() {
+    document.getElementsByClassName("main_container_slider").innerHTML = "<img src='image/main4.jpg'>";
+}
+function change_pic5() {
+    document.getElementsByClassName("main_container_slider").innerHTML = "<img src='image/main5.jpg'>";
+}
+function change_pic6() {
+    document.getElementsByClassName("main_container_slider").innerHTML = "<img src='image/main6.jpg'>";
+}
+function change_pic7() {
+    document.getElementsByClassName("main_container_slider").innerHTML = "<img src='image/main7.png'>";
+}
+function change_pic8() {
+    document.getElementsByClassName("main_container_slider").innerHTML = "<img src='image/main8.png'>";
+}
+function change_pic9() {
+    document.getElementsByClassName("main_container_slider").innerHTML = "<img src='image/main9.png'>";
+}
+function change_pic10() {
+    document.getElementsByClassName("main_container_slider").innerHTML = "<img src='image/main10.png'>";
+}
+function change_pic11() {
+    document.getElementsByClassName("main_container_slider").innerHTML = "<img src='image/main11.jpg'>";
+}
+function change_pic12() {
+    document.getElementsByClassName("main_container_slider").innerHTML = "<img src='image/main12.png'>";
+}
