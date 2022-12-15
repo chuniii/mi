84eebc569f314203909186813ee95a85a2c0837a
chuniii <10846033@ntub.edu.tw>
2022-12-15 20:18:40 +0800

    12150818

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+// header下拉選單
 const ProductType = {
     XIAOMI_PHONE: 0,
     REDMI_PHONE: 1,
@@ -7,6 +9,22 @@ const ProductType = {
     XIAOMI_ACCESSORIES: 5,
 }
 
+
+// main_choose_list右側選單
+const MainProductType = {
+    cellphone: 0,
+    entertainment: 1,
+    smart_family_machine: 2,
+    tour: 3,
+    main_smart_tech: 4,
+    threec_accessories: 5,
+    earphone: 6,
+    healthy_care: 7,
+    life: 8,
+}
+
+
+// main圖片更換
 function change_pic1() {
     document.getElementById("carouselsLink").innerHTML = "<img src='image/double12.png'>";
 }
@@ -45,6 +63,7 @@ function change_pic12() {
     document.getElementById("carouselsLink").innerHTML = "<img src='image/main12.png'>";
 }
 
+// onmouseenter 顯示header下拉區域
 function showProductMenu(type) {
     switch (type) {
         case ProductType.XIAOMI_PHONE:
@@ -70,6 +89,7 @@ function showProductMenu(type) {
     }
 }
 
+// onmouseleave 隱藏header下拉區域
 function hideProductMenu(type) {
     switch (type) {
         case ProductType.XIAOMI_PHONE:
@@ -94,3 +114,77 @@ function hideProductMenu(type) {
             break;
     }
 }
+
+
+// onmouseenter 顯示main_choose_list下拉區域
+function showMainProductMenu(type) {
+    switch (type) {
+        case MainProductType.cellphone:
+            document.getElementById("dropMenu_cellphone").style.display = "block";
+            break;
+        case MainProductType.entertainment:
+            document.getElementById("dropMenu_entertainment").style.display = "block";
+            break;
+        case MainProductType.smart_family_machine:
+            document.getElementById("dropMenu_smart_family_machine").style.display = "block";
+            break;
+        case MainProductType.tour:
+            document.getElementById("dropMenu_tour").style.display = "block";
+            break;
+        case MainProductType.main_smart_tech:
+            document.getElementById("dropMenu_main_smart_tech").style.display = "block";
+            break;
+        case MainProductType.threec_accessories:
+            document.getElementById("dropMenu_threec_accessories").style.display = "block";
+            break;
+        case MainProductType.earphone:
+            document.getElementById("dropMenu_earphone").style.display = "block";
+            break;
+        case MainProductType.healthy_care:
+            document.getElementById("dropMenu_healthy_care").style.display = "block";
+            break;
+        case MainProductType.life:
+            document.getElementById("dropMenu_life").style.display = "block";
+            break;
+        default:
+            break;
+    }
+}
+
+// onmouseleave 隱藏main_choose_list下拉區域
+function hideMainProductMenu(type) {
+    switch (type) {
+        case MainProductType.cellphone:
+            document.getElementById("dropMenu_cellphone").style.display = "none";
+            break;
+        case MainProductType.entertainment:
+            document.getElementById("dropMenu_entertainment").style.display = "none";
+            break;
+        case MainProductType.smart_family_machine:
+            document.getElementById("dropMenu_smart_family_machine").style.display = "none";
+            break;
+        case MainProductType.tour:
+            document.getElementById("dropMenu_tour").style.display = "none";
+            break;
+        case MainProductType.main_smart_tech:
+            document.getElementById("dropMenu_main_smart_tech").style.display = "none";
+            break;
+        case MainProductType.threec_accessories:
+            document.getElementById("dropMenu_threec_accessories").style.display = "none";
+            break;
+        case MainProductType.earphone:
+            document.getElementById("dropMenu_earphone").style.display = "none";
+            break;
+        case MainProductType.healthy_care:
+            document.getElementById("dropMenu_healthy_care").style.display = "none";
+            break;
+        case MainProductType.life:
+            document.getElementById("dropMenu_life").style.display = "none";
+            break;
+        default:
+            break;
+    }
+}
+
+
+
